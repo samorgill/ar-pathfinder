1c77c80cf8181ab0728fad2dbdae16972ec6740e
sam.orgill <sam.orgill@ultra-as.com>
2018-06-07 12:41:55 +0100

    ios

--- script.js
+++ script.js
@@ -64,100 +64,103 @@
 //         alert('Sorry, your browser does not support getUserMedia');
 //     }
 // }
+
+
 // 2nd App
-// $(function () {
-//     video = document.getElementById('camera-stream');
-//     video.style.width = document.width + 'px';
-//     video.style.height = document.height + 'px';
-//     video.setAttribute('autoplay', '');
-//     video.setAttribute('muted', '');
-//     video.setAttribute('playsinline', '');
+$(function () {
+    video = document.getElementById('camera-stream');
+    video.style.width = document.width + 'px';
+    video.style.height = document.height + 'px';
+    video.setAttribute('autoplay', '');
+    video.setAttribute('muted', '');
+    video.setAttribute('playsinline', '');
+
+    let constraints = {
+        audio: false,
+        video: {
+            facingMode: 'environment'
+        }
+    }
+
+    navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
+        video.srcObject = stream;
+    });
+});
+
+// var videoSelect = document.querySelector("select#videoSource");
+// var selectors = [videoSelect];
 //
-//     let constraints = {
-//         audio: false,
+// function gotDevices(deviceInfos) {
+//     // Handles being called several times to update labels. Preserve values.
+//     var values = selectors.map(function(select) {
+//         return select.value;
+//     });
+//     selectors.forEach(function(select) {
+//         while (select.firstChild) {
+//             select.removeChild(select.firstChild);
+//         }
+//     });
+//
+//     for (var i = 0; i !== deviceInfos.length; ++i) {
+//         var deviceInfo = deviceInfos[i];
+//         var option = document.createElement("option");
+//         option.value = deviceInfo.deviceId;
+//
+//         if (deviceInfo.kind === "videoinput") {
+//             option.text = deviceInfo.label || "camera " + (videoSelect.length + 1);
+//             videoSelect.appendChild(option);
+//         } else {
+//             console.log("Some other kind of source/device: ", deviceInfo);
+//         }
+//
+//         selectors.forEach(function(select, selectorIndex) {
+//             if (
+//                 Array.prototype.slice.call(select.childNodes).some(function(n) {
+//                     return n.value === values[selectorIndex];
+//                 })
+//             ) {
+//                 select.value = values[selectorIndex];
+//             }
+//         });
+//     }
+// }
+//
+// navigator.mediaDevices
+//     .enumerateDevices()
+//     .then(gotDevices)
+//     .catch(handleError);
+//
+// function gotStream(stream) {
+//     arToolkitSource.domElement.srcObject = stream; // make stream available to console
+//     // video.srcObject = stream;
+//     // Refresh button list in case labels have become available
+//     return navigator.mediaDevices.enumerateDevices();
+// }
+//
+// function start() {
+//     if (window.stream) {
+//         window.stream.getTracks().forEach(function(track) {
+//             track.stop();
+//         });
+//     }
+//     var videoSource = videoSelect.value;
+//     var constraints = {
 //         video: {
+//             deviceId: videoSource ? { exact: videoSource } : undefined,
 //             facingMode: 'environment'
 //         }
-//     }
+//     };
+//     navigator.mediaDevices
+//         .getUserMedia(constraints)
+//         .then(gotStream)
+//         .then(gotDevices)
+//         .catch(handleError);
+// }
 //
-//     navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
-//         video.srcObject = stream;
-//     });
-// });
-
-var videoSelect = document.querySelector("select#videoSource");
-var selectors = [videoSelect];
-
-function gotDevices(deviceInfos) {
-    // Handles being called several times to update labels. Preserve values.
-    var values = selectors.map(function(select) {
-        return select.value;
-    });
-    selectors.forEach(function(select) {
-        while (select.firstChild) {
-            select.removeChild(select.firstChild);
-        }
-    });
-
-    for (var i = 0; i !== deviceInfos.length; ++i) {
-        var deviceInfo = deviceInfos[i];
-        var option = document.createElement("option");
-        option.value = deviceInfo.deviceId;
-
-        if (deviceInfo.kind === "videoinput") {
-            option.text = deviceInfo.label || "camera " + (videoSelect.length + 1);
-            videoSelect.appendChild(option);
-        } else {
-            console.log("Some other kind of source/device: ", deviceInfo);
-        }
-
-        selectors.forEach(function(select, selectorIndex) {
-            if (
-                Array.prototype.slice.call(select.childNodes).some(function(n) {
-                    return n.value === values[selectorIndex];
-                })
-            ) {
-                select.value = values[selectorIndex];
-            }
-        });
-    }
-}
-
-navigator.mediaDevices
-    .enumerateDevices()
-    .then(gotDevices)
-    .catch(handleError);
-
-function gotStream(stream) {
-    arToolkitSource.domElement.srcObject = stream; // make stream available to console
-    // video.srcObject = stream;
-    // Refresh button list in case labels have become available
-    return navigator.mediaDevices.enumerateDevices();
-}
-
-function start() {
-    if (window.stream) {
-        window.stream.getTracks().forEach(function(track) {
-            track.stop();
-        });
-    }
-    var videoSource = videoSelect.value;
-    var constraints = {
-        video: {
-            deviceId: videoSource ? { exact: videoSource } : undefined
-        }
-    };
-    navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(gotStream)
-        .then(gotDevices)
-        .catch(handleError);
-}
-
-videoSelect.onchange = start;
-
-function handleError(error) {
-    console.log("navigator.getUserMedia error: ", error);
-}
-
-start();
+// videoSelect.onchange = start;
+//
+// function handleError(error) {
+//     console.log("navigator.getUserMedia error: ", error);
+// }
+//
+//start();
