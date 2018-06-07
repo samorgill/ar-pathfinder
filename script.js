9cf744e15f9ceaa75181ba83a20898647caf38e9
sam.orgill <sam.orgill@ultra-as.com>
2018-06-07 13:04:28 +0100

    ios

--- script.js
+++ script.js
@@ -67,25 +67,25 @@
 
 
 // 2nd App
-// $(function () {
-//     video = document.getElementById('camera-stream');
-//     video.style.width = document.width + 'px';
-//     video.style.height = document.height + 'px';
-//     video.setAttribute('autoplay', '');
-//     video.setAttribute('muted', '');
-//     video.setAttribute('playsinline', '');
-//
-//     let constraints = {
-//         audio: false,
-//         video: {
-//             facingMode: 'environment'
-//         }
-//     }
-//
-//     navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
-//         video.srcObject = stream;
-//     });
-// });
+$(function () {
+    video = document.getElementById('camera-stream');
+    video.style.width = document.width + 'px';
+    video.style.height = document.height + 'px';
+    video.setAttribute('autoplay', '');
+    video.setAttribute('muted', '');
+    video.setAttribute('playsinline', '');
+
+    let constraints = {
+        audio: false,
+        video: {
+            facingMode: 'environment'
+        }
+    }
+
+    navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
+        video.srcObject = stream;
+    });
+});
 
 
 // 3rd
