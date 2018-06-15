bb7f94ddb99e183839f56bcfba4299afa53a194f
vitali.kolontko <vitali.kolontko@ultra-as.com>
2018-06-15 10:01:48 +0100

    added location display

--- script.js
+++ script.js
@@ -77,8 +77,25 @@ function run(){
 };
 
 function updateLocation(){
-    document.getElementById("myLocation").innerHTML = infoWindow.getCurrentPosition().lat() + ' ' + infoWindow.getCurrentPosition().lng()   // display location on screen
-}
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            let pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            document.getElementById("myLocation").innerHTML = pos.lat + ' ' + pos.lng   // display location on screen
+
+
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+    }
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
