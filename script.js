c5eec442ed0a27d52dfd3f300237dff7c57622a4
vitali.kolontko <vitali.kolontko@ultra-as.com>
2018-06-15 15:11:55 +0100

    created angle

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ function run(){
             let locX = p2.x - p1.x;
 
             console.log("Heading: " + heading);
-            document.getElementById("ascene").childNodes[9].setAttribute('position',{x: locX, y: locY, z: -6});
+            document.getElementById("ascene").childNodes[9].setAttribute('position',{x: locX, y: 0, z: locY});
             document.getElementById("ascene").childNodes[9].setAttribute('rotation',{x: -90, y: heading, z: 0});
             //document.getElementById("ascene").childNodes[13].setAttribute('position',{x: 0, y: 0, z: nodeDistance*1000000});
 
